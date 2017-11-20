df841fe265b88bd434f44e2d6900b20d907c7100
kflange <zknsftk1msrk@gmail.com>
2017-11-20 14:38:10 +0900

    output read-file to console.log

--- src/main/main.ts
+++ src/main/main.ts
@@ -7,11 +7,11 @@ import {ipcMain} from "electron";
 import {show_open_file_dialog} from "./showOpenFileDialog";
 
 let mainWindow: MainWindow | null;
-let fileManger: FileManager;
+let fileManager: FileManager;
 
 app.on('ready', () => {
   mainWindow = createMainWindow();
-  fileManger = createFileManager();
+  fileManager = createFileManager();
 });
 
 
@@ -31,5 +31,9 @@ app.on('activate', () => {
 
 ipcMain.on("open-file-dialog", (event: Event) => {
   console.log("openFile");
-  show_open_file_dialog();
+  show_open_file_dialog()
+    .then((filepath: string) => {return fileManager.read_file(filepath)})
+    .then((text: string) => {console.log(text)})
+    //.then((text: string) => {mainWindow.sendText(text)})
+    .catch((error) => {console.log(error)});
 });
