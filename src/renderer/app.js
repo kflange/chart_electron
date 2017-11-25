da4dfde8077f2adec8455bb59a0766d3ca395a77
kflange <zknsftk1msrk@gmail.com>
2017-11-26 01:53:48 +0900

    fix

--- src/renderer/app.js
+++ src/renderer/app.js
@@ -1,5 +1,9 @@
+const riot = require("riot");
+
 require('./tag/app.tag');
 require('./tag/hello.tag');
 require('./tag/button_openfile.tag');
 require('./tag/itemlist.tag');
 require('./tag/mychart.tag');
+
+riot.mount("*");
