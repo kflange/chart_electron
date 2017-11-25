9a81279f7a8f3f4643b975943a8351a700f2d671
kflange <zknsftk1msrk@gmail.com>
2017-11-26 02:17:49 +0900

    fix to webpack

--- src/renderer/app.js
+++ src/renderer/app.js
@@ -1,9 +1,9 @@
 const riot = require("riot");
 
-require('./tag/app.tag');
-require('./tag/hello.tag');
-require('./tag/button_openfile.tag');
-require('./tag/itemlist.tag');
-require('./tag/mychart.tag');
+import app from './tag/app.tag';
+import hello from './tag/hello.tag';
+import button_openfile from './tag/button_openfile.tag';
+import itemlist from './tag/itemlist.tag';
+import mychart from './tag/mychart.tag';
 
 riot.mount("*");
